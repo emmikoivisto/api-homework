bb35727c1ad342c6ba0abb17eeb7e8847f21659a
emmikoivisto <emetsi@gmail.com>
2020-12-08 16:32:15 +0000

    fetch working

--- src/containers/SongsContainer.js
+++ src/containers/SongsContainer.js
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 import SongDetail from "../components/SongDetail";
 
 const SongsContainer = () => {
+
+    const [songs, setSongs] = useState([]);
+
+    const getSongs = () => {
+        console.log("getting songs")
+        fetch("https://itunes.apple.com/gb/rss/topsongs/limit=20/json")
+        .then(res => res.json())
+        .then(data => setSongs(data))
+    }
+
+    useEffect(() => {
+        getSongs();
+    }, [])
+
+
     return (
         <>
             <p>This is the song container</p>
